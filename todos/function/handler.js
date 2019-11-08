39c10aa389d49494f07a511400d86b567ac8b159
pasaperez <pasaperez@yahoo.com.ar>
2019-11-08 01:07:18 -0300

    Test de parametros con evento

--- todos/function/handler.js
+++ todos/function/handler.js
@@ -2,15 +2,16 @@
 
 module.exports = (context, callback) => 
 {
-    console.log(process.env);
-    todos(context)
-    callback(undefined, {status: "done"});
+	console.log(process.env);
+	console.log(context);
+	var fin=todos(context);
+	callback(undefined, fin);
 }
 
 function todos(context)
 {
 	var Minio = require('minio');
-
+	
 	var minioClient = new Minio.Client
 	({
 		endPoint: 'minio-service-minio.apps.us-west-1.starter.openshift-online.com',
@@ -19,28 +20,6 @@ function todos(context)
 		accessKey: 'minio',
 		secretKey: 'minio123'
 	});
-    
-    var nameBucket= context;
-    
-    minioClient.bucketExists(nameBucket, function(err, exists) 
-	{
-	  if (err) 
-	  {
-		return console.log(err)
-	  }
-	  if (exists) 
-	  {
-		return console.log('Bucket exists.');
-	  }
-	  else
-	  {
-		minioClient.makeBucket(nameBucket, 'us-east-1', function(err) 
-		{
-		  if (err) return console.log('Error creating bucket.', err)
-		  console.log('Bucket created successfully in "us-east-1".')
-		})
-      }
-	}
-	);
-    
+	
+	return {status: "done"};
 }
