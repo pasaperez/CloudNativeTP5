6ed0d525b0bfd017da8ecdc8601b4b58f7a9b371
pasaperez <pasaperez@yahoo.com.ar>
2019-11-13 00:31:56 -0300

    Update handler.js

--- todos/function/handler.js
+++ todos/function/handler.js
@@ -5,6 +5,10 @@ module.exports = (context, callback) =>
 	console.log(process.env);
 	console.log(context);
 	var fin=todos(context);
+	
+	var resultado={entorno: process.env, contexto=context, resultadofinal: fin}
+	guardar(resultado);
+	
 	callback(undefined, fin);
 }
 
@@ -22,4 +26,25 @@ function todos(context)
 	});
 	
 	return {status: "done"};
+}
+
+function guardar(objeto)
+{
+	const MongoClient = require('mongodb').MongoClient;
+
+	const dbd="minio";
+	const uri = "mongodb+srv://usertest:VuheioW9z1pMazuC@pasaperez-vzf9m.gcp.mongodb.net/"+dbd+"?w=majority";
+	const client = new MongoClient(uri, {useNewUrlParser: true,useUnifiedTopology: true});
+	const coll="log";
+	var objet=objeto;
+
+	client.connect(err => 
+	{
+	  const collection = client.db(dbd).collection(coll).insertOne(objet, function(err, res)
+	  {
+	    if (err) throw err;
+	    console.log("1 documento insertado");
+	  });
+	  client.close();
+	});
 }
