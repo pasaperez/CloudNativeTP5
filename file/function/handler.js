768d46f6fbf7d8e81f66c4383874ec948d5131a5
pasaperez <pasaperez@yahoo.com.ar>
2019-11-18 15:14:30 -0300

    file test

--- file/function/handler.js
+++ file/function/handler.js
@@ -2,9 +2,46 @@
 
 module.exports = (context, callback) => 
 {
-	console.log("1");
+	if (process.env.Http_Path!='/')
+	{
+		
+	}
+	else
+	{
+		var consulta="key";
+	}
+	
+	switch(consulta)
+	{
+		case "key":
+		{
+			encontrar({}, "todos");
+		}
+	}
+	
 	console.log(process.env);
-	console.log("2");
 	console.log(context);
 	callback(undefined, {status: "Done"});
+}
+
+function encontrar(consulta,coll)
+{
+	const MongoClient = require('mongodb').MongoClient;
+
+	const dbd="minio";
+	const uri = "mongodb+srv://usertest:VuheioW9z1pMazuC@pasaperez-vzf9m.gcp.mongodb.net/"+dbd+"?w=majority";
+	const client = new MongoClient(uri, {useNewUrlParser: true,useUnifiedTopology: true});
+	if (consulta=={})
+	{
+		client.connect(err => 
+		{
+		  const collection = client.db(dbd).collection(coll).find({}).addQueryModifier('$orderby',
+			  {a:1}).toArray(function(err, docs)
+			  {
+			    console.log(docs);
+			    console.log("\n");
+			  });
+		  client.close();
+		});
+	}		
 }
