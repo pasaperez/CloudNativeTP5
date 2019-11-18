55271a63be4d1fc80eec8360989a1423b7e9c31f
pasaperez <pasaperez@yahoo.com.ar>
2019-11-18 02:43:31 -0300

    Final Todos 1.1

--- todos/function/handler.js
+++ todos/function/handler.js
@@ -30,8 +30,8 @@ function todos(nameObjec, peso, tipo, fecha, callback)
 	
 	var minioClient = new Minio.Client
 	({
-		endPoint: 'http://192.168.106.134',
-		port:30381,
+		endPoint: 'http://minio-service-minio.apps.us-west-1.starter.openshift-online.com',
+		port:80,
 		useSSL: false,
 		accessKey: 'minio',
 		secretKey: 'minio123'
